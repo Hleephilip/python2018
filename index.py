74e0a98f638cfc3d1d2c66e9dfb71a5621dd715b
frogyunmax <frogyunmax@gmail.com>
2018-06-14 13:11:59 +0900

    코드 순서

--- index.py
+++ index.py
@@ -1,1 +1,8 @@
-"print('hello world')" 
+1. 지역에 따른 CCTV 목적별로 그래프 그리기
+   : CCTV_purpose.py
+2. 지역에 따른 CCTV 설치 연도별로 그래프 그리기
+   : CCTV_year.py
+3. 지역에 따른 연도별 범죄율 + 서울시 평균
+   : SIN_year.py
+4. 2, 3번 종합
+   : CCTV_SIN_comb.py
